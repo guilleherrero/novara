a4c754a90b4d6fa8847a71299acbefa449d7eb70
guilleherrero <62477759+guilleherrero@users.noreply.github.com>
2025-07-10 00:55:00 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Dashboard } from './components/Dashboard';
+import Dashboard from './Dashboard'; // ✅ nuevo nombre correcto
 
-function App() {
-  return <Dashboard />;
-}
+const App = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
 
 export default App;
